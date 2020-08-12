bbe4f30404c383d8e41e618535e6877c503831b3
Riley Slayden <slaydenriley@gmail.com>
2020-08-12 14:52:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 function saturdayFun(activity = "roller-skate") {
   return `This Saturday, I want to ${activity}!`;
 }
+
+function mondayWork(work = "go to the office") {
+  return `This Monday, I will ${work}.`;
+}
